032d1944c95f0a7a0bea5631fdd52a692099719a
Marco Graziotti <marco.graziotti@codingops.dev>
2025-02-24 16:08:48 +0100

    Added basic routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './home/home.component';
+import { ReservationFormComponent } from './reservation-form/reservation-form.component';
+import { ReservationListComponent } from './reservation-list/reservation-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent }, // Home route
+  { path: 'list', component: ReservationListComponent }, // Reservation List route
+  { path: 'new', component: ReservationFormComponent }, // New Reservation route
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
