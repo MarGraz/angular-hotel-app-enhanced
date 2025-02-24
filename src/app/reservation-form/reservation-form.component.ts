6e4ec2a5c22dfa8fa3fc63733743c125f9fa97f9
Marco Graziotti <marco.graziotti@codingops.dev>
2025-02-24 17:43:14 +0100

    In the reservation form component added a constructor, and validators inside ngOnInit. Added also on the submit button the possibility to turn off the button if all the required fields are not filled

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -7,13 +7,28 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './reservation-form.component.html',
   styleUrl: './reservation-form.component.css',
 })
-export class ReservationFormComponent {
+
+// Implement OnInit to perform the validation
+export class ReservationFormComponent implements OnInit {
   // New empty form group, it will be populated using FormBuilder
   reservationForm: FormGroup = new FormGroup({});
 
-  // Method triggered when the form is submitted
-  onSubmit() {
+  // It will be invoked every time this component is instantiated
+  constructor(private formBuilder: FormBuilder) {}
 
+  ngOnInit(): void {
+    this.reservationForm = this.formBuilder.group({
+      checkInDate: ['', Validators.required],
+      checkOutDate: ['', Validators.required],
+      guestName: ['', Validators.required],
+      guestEmail: ['', [Validators.required, Validators.email]],
+      roomNumber: ['', Validators.required],
+    });
   }
 
+  // Method triggered when the form is submitted
+  onSubmit() {
+    if (this.reservationForm.valid) {
+    }
+  }
 }
