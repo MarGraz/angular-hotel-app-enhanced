8c54651b18a459b463055896b0f1aaf08fa7b177
Marco Graziotti <marco.graziotti@codingops.dev>
2025-02-24 16:44:29 +0100

    Added the first part of the reactive form validation, to perform the validation inside the ts file

--- src/app/reservation-form/reservation-form.component.ts
+++ src/app/reservation-form/reservation-form.component.ts
@@ -1,11 +1,16 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-reservation-form',
   standalone: false,
   templateUrl: './reservation-form.component.html',
-  styleUrl: './reservation-form.component.css'
+  styleUrl: './reservation-form.component.css',
 })
 export class ReservationFormComponent {
+  // New empty form group, it will be populated using FormBuilder
+  reservationForm: FormGroup = new FormGroup({});
+
+  
 
 }
